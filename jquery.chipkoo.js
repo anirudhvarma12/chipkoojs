3e746824b7090751cf7dbba2b247b037f61abc76
Anirudh <varma93@hotmail.com>
2014-10-02 14:18:37 +0530

    Initial commit, with plugin working minus styles.

---
+++ jquery.chipkoo.js
@@ -0,0 +1,22 @@
+(function ($) {
+    $.fn.chipkoo = function () {
+        var $head = $(this).find("tr:first-child");
+        var $dataRow = $(this).find("tr").eq(1);
+        var lengthArray = new Array();
+        $dataRow.children().each(function () {
+            lengthArray.push($(this).outerWidth());
+        });
+        for (var i = 0; i < lengthArray.length; i++) {
+            $head.children().eq(i).innerWidth(lengthArray[i]);
+        }
+        $head.css({
+            "position": "fixed"
+        });
+        var height = $head.outerHeight();
+        var className = "js-chipkoo";
+        $("<tr class='" + className + "'></tr>").insertAfter($head);
+        $("." + className).height(height);
+        $("." + className).css("visiblity", "hidden");
+        return this;
+    };
+}(jQuery));
